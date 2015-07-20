0a6022e9e6f47f042eecae0dd87167ca483dac58
Jorge L Pérez <jolisper@gmail.com>
2015-07-20 03:40:10 -0300

    Change package repositories

--- package-archives.el
+++ package-archives.el
@@ -1,5 +1,4 @@
 ;; Repository Packages
 (setq package-archives
-      '(("gnu" . "http://elpa.gnu.org/packages/")
-	("marmalade" . "http://marmalade-repo.org/packages/")
-	("melpa" . "http://melpa.milkbox.net/packages/")))
+      '(("gnu"   . "http://elpa.gnu.org/packages/")
+	("melpa" . "http://stable.melpa.org/packages/")))
