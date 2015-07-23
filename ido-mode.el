2977493fc1a039409f1d409194677ed2d88afe4d
Jorge L Pérez <jolisper@gmail.com>
2015-07-23 01:39:16 -0300

    Added command completion with ido-mode

--- ido-mode.el
+++ ido-mode.el
@@ -1,3 +1,13 @@
 (ido-mode 1)
 (setq ido-everywhere t)
 (setq ido-enable-flex-matching t)
+
+(global-set-key
+     "\M-x"
+     (lambda ()
+       (interactive)
+       (call-interactively
+        (intern
+         (ido-completing-read
+          "M-x "
+          (all-completions "" obarray 'commandp))))))
