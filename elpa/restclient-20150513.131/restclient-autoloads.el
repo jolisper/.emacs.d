b22e7705c1572d9b5894c75d5a114a624934273a
Jorge Perez <jorge.perez@avantrip.com>
2015-08-04 20:36:26 -0300

    Added restclient mode

---
+++ elpa/restclient-20150513.131/restclient-autoloads.el
@@ -0,0 +1,45 @@
+;;; restclient-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+
+;;;### (autoloads (restclient-mode restclient-http-send-current-stay-in-window
+;;;;;;  restclient-http-send-current-raw restclient-http-send-current)
+;;;;;;  "restclient" "restclient.el" (21953 7286 174070 223000))
+;;; Generated autoloads from restclient.el
+
+(autoload 'restclient-http-send-current "restclient" "\
+
+
+\(fn &optional RAW STAY-IN-WINDOW)" t nil)
+
+(autoload 'restclient-http-send-current-raw "restclient" "\
+
+
+\(fn)" t nil)
+
+(autoload 'restclient-http-send-current-stay-in-window "restclient" "\
+
+
+\(fn)" t nil)
+
+(autoload 'restclient-mode "restclient" "\
+
+
+\(fn)" t nil)
+
+;;;***
+
+;;;### (autoloads nil nil ("restclient-pkg.el") (21953 7286 257913
+;;;;;;  519000))
+
+;;;***
+
+(provide 'restclient-autoloads)
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; restclient-autoloads.el ends here
