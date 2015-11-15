bfe849d3f3c81589c8a500e83b025b6ace81645a
Jorge Luis Perez <jorge.perez@avantrip.com>
2015-11-15 00:52:05 -0300

    Change font for mac

--- mac-tunning.el
+++ mac-tunning.el
@@ -1,5 +1,7 @@
 (when (eq system-type 'darwin)
-(setq mac-option-key-is-meta nil
-      mac-command-key-is-meta t
-      mac-command-modifier 'meta
-      mac-option-modifier 'none))
+  (setq mac-option-key-is-meta nil
+	mac-command-key-is-meta t
+	mac-command-modifier 'meta
+	mac-option-modifier 'none)
+  (set-face-attribute 'default nil
+		      :family "Monaco" :height 120 :weight 'normal))
