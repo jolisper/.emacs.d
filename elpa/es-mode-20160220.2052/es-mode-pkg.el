38f06d52636a1d2410e5c47de033cdb29e6f477a
Jorge Luis Pérez <jolisper@gmail.com>
2016-03-28 01:25:38 -0300

    Elastic Search mode added

---
+++ elpa/es-mode-20160220.2052/es-mode-pkg.el
@@ -0,0 +1,8 @@
+(define-package "es-mode" "20160220.2052" "A major mode for editing Elasticsearch queries"
+  '((dash "2.11.0")
+    (cl-lib "0.5"))
+  :url "http://www.github.com/dakrone/es-mode" :keywords
+  '("elasticsearch"))
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
