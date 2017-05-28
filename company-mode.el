8018bdaaec0f112097d6dd6b68ceb3ba6f17343c
Jorge Luis Pérez <jolisper@gmail.com>
2017-05-28 12:48:23 -0300

    Company mode config tunning

--- company-mode.el
+++ company-mode.el
@@ -1,6 +1,7 @@
-(add-hook 'after-init-hook 'global-company-mode)
+(global-company-mode)
                                         
 (setq company-tooltip-limit 20)                      ; bigger popup window
 (setq company-idle-delay .3)                         ; decrease delay before autocompletion popup shows
 (setq company-echo-delay 0)                          ; remove annoying blinking
 (setq company-begin-commands '(self-insert-command)) ; start autocompletion only after typing
+(setq company-minimum-prefix-length 1)
